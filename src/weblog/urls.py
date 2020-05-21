c594b65391a1ce34d34157054583497ea5e74813
pashamakhilkumarreddy <akhil.reddy@frequence.com>
2020-05-21 20:35:36 +0530

    add post model

--- src/weblog/urls.py
+++ src/weblog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home_view, about_view
+from .views import home_view, about_view, login_view, register_view
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('about', about_view, name='about'),
+    path('login', login_view, name='login'),
+    path('register', register_view, name='register'),
 ]
--- src/weblog/views.py
+++ src/weblog/views.py
@@ -1,32 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-  {
-    'author': 'Stacy',
-    'title': 'Blog Post 1',
-    'content': 'First Post',
-    'date_posted': 'August 27, 2018'
-  },
-  {
-    'author': 'John Doe',
-    'title': 'Blog Post 2',
-    'content': 'First Post',
-    'date_posted': 'August 27, 2019'
-  },
-  {
-    'author': 'Stacy',
-    'title': 'Blog Post 3',
-    'content': 'First Post',
-    'date_posted': 'August 27, 2020'
-  }
-]
+from .models import Posts
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
+  posts = Posts.objects.all()
   return render(request, 'blog/index.html', {
     'posts': posts
   })
 
 def about_view(request, *args, **kwargs):
   return render(request, 'blog/about.html')
+
+def login_view(request, *args, **kwargs):
+  return render(request, 'blog/auth/login.html')
+
+def register_view(request, *args, **kwargs):
+  return render(request, 'blog/auth/register.html')
